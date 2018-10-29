31ff7c4bd7b77563de975115efc3fe99e08c5440
Y.Baghaee <unos.bm65@gmail.com>
2018-10-29 10:29:16 +0330

    a_change

--- Samples_NetCore/AspNetCoreSample/Controllers/ValuesController.cs
+++ Samples_NetCore/AspNetCoreSample/Controllers/ValuesController.cs
@@ -10,6 +10,7 @@ namespace AspNetCoreSample.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
