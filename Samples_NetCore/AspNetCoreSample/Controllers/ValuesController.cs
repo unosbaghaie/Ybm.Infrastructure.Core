3e4019be24557d301f55e509f6573a3b4c1ac043
Y.Baghaee <unos.bm65@gmail.com>
2018-10-29 12:46:00 +0330

    TestBuild

--- Samples_NetCore/AspNetCoreSample/Controllers/ValuesController.cs
+++ Samples_NetCore/AspNetCoreSample/Controllers/ValuesController.cs
@@ -10,14 +10,12 @@ namespace AspNetCoreSample.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
-
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
